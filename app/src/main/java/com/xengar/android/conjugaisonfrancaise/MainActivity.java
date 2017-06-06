ba2f28c54060b54766f33118e64cbdb84c48a409
Angel Newton <xanewton@gmail.com>
2017-06-06 14:27:58 -0500

    Add readme

--- app/src/main/java/com/xengar/android/conjugaisonfrancaise/MainActivity.java
+++ app/src/main/java/com/xengar/android/conjugaisonfrancaise/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 Angel Garcia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.xengar.android.conjugaisonfrancaise;
 
 import android.os.Bundle;
@@ -13,6 +28,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * MainActivity
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
